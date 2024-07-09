2947441452a30a55ece062bb3e56e2a778f3cf27
Katie Ludwig <katierludwig@gmail.com>
2024-07-08 20:11:50 -0700

    wip

--- api/data/db-config.js
+++ api/data/db-config.js
@@ -1,7 +1,7 @@
 const knex = require('knex');
 const configurations = require('../../knexfile');
 
-const environment = process.env.NODE_ENV || 'development';
+const environment = process.env.NODE_ENV
 const db = knex(configurations[environment]);
 
 module.exports = db;
